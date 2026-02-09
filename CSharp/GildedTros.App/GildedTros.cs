a0524efb814950b40d9380c626254a05b803bb5e
Kristof <Kristof@DESKTOP-5DS90R7>
2026-02-09 20:22:47 +0100

    Introduce factory design pattern

--- CSharp/GildedTros.App/GildedTros.cs
+++ CSharp/GildedTros.App/GildedTros.cs
@@ -4,11 +4,6 @@ namespace GildedTros.App
 {
     public class GildedTros
     {
-        private const string GoodWine = "Good Wine";
-        private const string BackstagePassesHaxx = "Backstage passes for HAXX";
-        private const string BackstagePassesRefactor = "Backstage passes for Re:factor";
-        private const string Keychain = "B-DAWG Keychain";
-
         IList<Item> Items;
 
         public GildedTros(IList<Item> Items)
@@ -18,51 +13,11 @@ namespace GildedTros.App
 
         public void UpdateQuality()
         {
+            ItemFactory factory = new();
+
             foreach (Item item in Items)
             {
-                switch (item.Name)
-                {
-                    case GoodWine:
-                        {
-                            if (item.Quality < MaxItemQuality)
-                                item.Quality++;
-
-                            item.SellIn--;
-
-                            if (item.SellIn < 0 && item.Quality < MaxItemQuality)
-                                item.Quality++;
-                        }
-                        break;
-                    case BackstagePassesHaxx:
-                    case BackstagePassesRefactor:
-                        {
-                            if (item.Quality < MaxItemQuality)
-                                item.Quality++;
-                            if (item.SellIn < 11)
-                                item.Quality++;
-                            if (item.SellIn < 6)
-                                item.Quality++;
-
-                            item.SellIn--;
-
-                            if (item.SellIn < 0)
-                                item.Quality = MinQuality;
-                        }
-                        break;
-                    case Keychain:
-                        break;
-                    default:
-                        {
-                            if (item.Quality > MinQuality)
-                                item.Quality--;
-
-                            item.SellIn--;
-
-                            if (item.SellIn < 0)
-                                item.Quality--;
-                        }
-                        break;
-                }
+                factory.CreateItem(item).Update();
             }
         }
     }
---
+++ CSharp/GildedTros.App/ItemFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace GildedTros.App
+{
+    public class ItemFactory
+    {
+        private const string GoodWine = "Good Wine";
+        private const string BackstagePassesHaxx = "Backstage passes for HAXX";
+        private const string BackstagePassesRefactor = "Backstage passes for Re:factor";
+        private const string Keychain = "B-DAWG Keychain";
+
+        public BaseItem CreateItem(Item item)
+        {
+            return item.Name switch
+            {
+                GoodWine => new GoodWineItem(item),
+                BackstagePassesHaxx => new BackstagePassesItem(item),
+                BackstagePassesRefactor => new BackstagePassesItem(item),
+                Keychain => new KeychainItem(item),
+                _ => new DefaultItem(item)
+            };
+        }
+    }
+}
