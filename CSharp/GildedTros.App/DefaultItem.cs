b954e3ed1f3d2877ef36d25c62aceff89add0b07
Kristof <Kristof@DESKTOP-5DS90R7>
2026-02-09 20:02:05 +0100

    Introduce DefaultItem class

---
+++ CSharp/GildedTros.App/DefaultItem.cs
@@ -0,0 +1,20 @@
+﻿namespace GildedTros.App
+{
+    public class DefaultItem : BaseItem
+    {
+        public DefaultItem(Item item) : base(item)
+        {
+        }
+
+        public override void Update()
+        {
+            if (item.Quality > GildedTros.MinItemQuality)
+                item.Quality--;
+
+            item.SellIn--;
+
+            if (item.SellIn < 0)
+                item.Quality--;
+        }
+    }
+}
