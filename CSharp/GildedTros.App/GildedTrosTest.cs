7cb6267a30e2632dbd74200fcf8447785e2afad7
Kristof <Kristof@DESKTOP-5DS90R7>
2026-02-09 10:57:14 +0100

    Add DecreaseQualityByOneEndOfDay test

--- CSharp/GildedTros.App/GildedTrosTest.cs
+++ CSharp/GildedTros.App/GildedTrosTest.cs
@@ -18,5 +18,19 @@ namespace GildedTros.App
             // Then SellIn should be 1
             Assert.Equal(1, Items[0].SellIn);
         }
+
+        [Fact]
+        public void DecreaseQualityByOneEndOfDay()
+        {
+            // Given an item with Quality of 2
+            IList<Item> Items = new List<Item> { new() { Name = "foo", SellIn = 2, Quality = 2 } };
+            GildedTros app = new(Items);
+
+            // When the quality update is processed end of day
+            app.UpdateQuality();
+
+            // Then Quality should be 1
+            Assert.Equal(1, Items[0].Quality);
+        }
     }
 }
