c3eb30cead9961eab5e9a4b547d0d3036f42b983
Kristof <Kristof@DESKTOP-5DS90R7>
2026-02-09 21:00:31 +0100

    Introduce UpdateQuality_Should_Decrease_Smelly_Item_Quality_By_2 test

--- CSharp/GildedTros.App/GildedTrosTest.cs
+++ CSharp/GildedTros.App/GildedTrosTest.cs
@@ -203,6 +203,21 @@ namespace GildedTros.App
             Assert.Equal(80, item.Quality);
         }
 
+        [Fact]
+        // Smelly items ("Duplicate Code", "Long Methods", "Ugly Variable Names") degrade in Quality twice as fast as normal items
+        public void UpdateQuality_Should_Decrease_Smelly_Item_Quality_By_2()
+        {
+            // Given a "Duplicate Code" item with SellIn of 10 and Quality of 30
+            var item = CreateItem("Duplicate Code", 10, 30);
+            var app = CreateGildedTros(item);
+
+            // When the quality update is processed end of day
+            app.UpdateQuality();
+
+            // Then the Quality should still be 80
+            Assert.Equal(28, item.Quality);
+        }
+
         private static Item CreateItem(string name, int sellIn, int quality)
         {
             return new Item()
