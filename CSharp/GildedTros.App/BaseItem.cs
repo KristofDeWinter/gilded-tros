08c1121d3df749125ee5de5b1b311698eea8ecf1
Kristof <Kristof@DESKTOP-5DS90R7>
2026-02-09 19:41:59 +0100

    Introduce abstract BaseItem

---
+++ CSharp/GildedTros.App/BaseItem.cs
@@ -0,0 +1,14 @@
+﻿namespace GildedTros.App
+{
+    public abstract class BaseItem
+    {
+        protected Item item;
+
+        public BaseItem(Item item)
+        {
+            this.item = item;
+        }
+
+        public abstract void Update();
+    }
+}
