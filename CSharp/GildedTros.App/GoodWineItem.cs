0b5d3e49a9eba114ac47e0a200e08e2d5a9c8086
Kristof <Kristof@DESKTOP-5DS90R7>
2026-02-09 19:47:50 +0100

    Introduce GoodWineItem class

---
+++ CSharp/GildedTros.App/GoodWineItem.cs
@@ -0,0 +1,20 @@
+﻿namespace GildedTros.App
+{
+    public class GoodWineItem : BaseItem
+    {
+        public GoodWineItem(Item item) : base(item)
+        {
+        }
+
+        public override void Update()
+        {
+            if (item.Quality < GildedTros.MaxItemQuality)
+                item.Quality++;
+
+            item.SellIn--;
+
+            if (item.SellIn < 0 && item.Quality < GildedTros.MaxItemQuality)
+                item.Quality++;
+        }
+    }
+}
