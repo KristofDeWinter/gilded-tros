937363ae07022b560502bf14a2ea3063ac3f9fdc
Kristof <Kristof@DESKTOP-5DS90R7>
2026-02-09 11:32:56 +0100

    Add IncreaseQualityGoodWineEndOfDayRemainsQualityFifty test

--- CSharp/GildedTros.App/GildedTrosTest.cs
+++ CSharp/GildedTros.App/GildedTrosTest.cs
@@ -75,5 +75,19 @@ namespace GildedTros.App
             Assert.Equal(1, Items[0].SellIn);
             Assert.Equal(3, Items[0].Quality);
         }
+
+        [Fact]
+        public void IncreaseQualityGoodWineEndOfDayRemainsQualityFifty()
+        {
+            // Given an item with SellIn of 5 and quality of 50
+            IList<Item> Items = new List<Item> { new() { Name = "Good Wine", SellIn = 5, Quality = 50 } };
+            GildedTros app = new(Items);
+
+            // When the quality update is processed end of day
+            app.UpdateQuality();
+
+            // Then Quality should still be 50
+            Assert.Equal(50, Items[0].Quality);
+        }
     }
 }
