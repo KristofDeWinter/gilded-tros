40ac7dfcc53a16958765899fae0e02c38ad2f7d6
Kristof <Kristof@DESKTOP-5DS90R7>
2026-02-09 19:52:54 +0100

    Introduce BackstagePassesItem class

---
+++ CSharp/GildedTros.App/BackstagePassesItem.cs
@@ -0,0 +1,24 @@
+﻿namespace GildedTros.App
+{
+    public class BackstagePassesItem : BaseItem
+    {
+        public BackstagePassesItem(Item item) : base(item)
+        {
+        }
+
+        public override void Update()
+        {
+            if (item.Quality < GildedTros.MaxItemQuality)
+                item.Quality++;
+            if (item.SellIn < 11)
+                item.Quality++;
+            if (item.SellIn < 6)
+                item.Quality++;
+
+            item.SellIn--;
+
+            if (item.SellIn < 0)
+                item.Quality = GildedTros.MinItemQuality;
+        }
+    }
+}
